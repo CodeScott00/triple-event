fec9fdd61dd835b342af92511b84c88a74c2192a
Skygod <dougal_00@hotmail.com>
2021-01-21 14:59:29 +0000

    second commit

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,10 +1,13 @@
 from app import app
 from flask import render_template
+from app.models.event_list import events
+from app.models.event import Event
+
 
 @app.route('/')
 def index():
     return "Hello World"
 
 @app.route('/events')
-def events():
-    return render_template "index.html"
+def events_list_all():
+    return render_template("index.html", events=events)
--- app/models/event_list.py
+++ app/models/event_list.py
@@ -1,7 +1,7 @@
 from app.models.event import Event
 
 event_1 = Event("Ethereum Event", "2021-1-22", 20, "SEC", "Im a do-er")
-event_2 = Event("Bankrupty Help Group", "2021-1-23", 30, "Basement", "Im not goner")
+event_2 = Event("Bankrupty Help Group", "2021-1-23", 30, "Basement", "Im a goner")
 
 events = [event_1,event_2]
 
